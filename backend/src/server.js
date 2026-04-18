6294c12bf5f71adb9cd0b0a058533276046762a2
Nguyễn Như Minh Đức <nguyennhuminhduc@Macbook-cua-Minh-uc.local>
2026-04-18 13:57:18 +0700

    n

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,8 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
 import path from "path";
+import fetch from "node-fetch";
+
 
 dotenv.config();
 
@@ -18,6 +20,69 @@ app.use(express.json());
 if (process.env.NODE_ENV !== "production") {
   app.use(cors({ origin: "http://localhost:5173" }));
 }
+// ===== API FOOTBALL =====
+
+// 🔍 Search player
+app.get("/api/players", async (req, res) => {
+  try {
+    const name = req.query.name;
+
+    const response = await fetch(
+      `https://v3.football.api-sports.io/players?search=${name}`,
+      {
+        headers: {
+          "x-apisports-key": process.env.API_KEY,
+        },
+      }
+    );
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Lỗi players" });
+  }
+});
+
+// ⚽ Match hôm nay
+app.get("/api/matches", async (req, res) => {
+  try {
+    const today = new Date().toISOString().split("T")[0];
+
+    const response = await fetch(
+      `https://v3.football.api-sports.io/fixtures?date=${today}`,
+      {
+        headers: {
+          "x-apisports-key": process.env.API_KEY,
+        },
+      }
+    );
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Lỗi matches" });
+  }
+});
+
+// 🏆 Standings
+app.get("/api/standings", async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://v3.football.api-sports.io/standings?league=39&season=2024`,
+      {
+        headers: {
+          "x-apisports-key": process.env.API_KEY,
+        },
+      }
+    );
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Lỗi standings" });
+  }
+});
+
 
 app.use("/api/tasks", tasksRoute);
 
@@ -34,12 +99,3 @@ connectDB().then(() => {
     console.log(`server bắt đầu trên cổng ${PORT}`);
   });
 });
-var myHeaders = new Headers();
-myHeaders.append("x-apisports-key", "XxXxXxXxXxXxXxXxXxXxXxXx");
-
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
-};
-
